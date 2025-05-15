c9c92e5724ab5dfc9acb64d02682c6e93353f6df
Pavel Bobylev <131748689+PBobylev@users.noreply.github.com>
2025-05-15 15:08:22 +0500

    MODLD-729: Rdf4LdService introduced (#10)

---
+++ src/main/java/org/folio/rdf4ld/service/Rdf4LdServiceImpl.java
@@ -0,0 +1,54 @@
+package org.folio.rdf4ld.service;
+
+import static java.util.Objects.isNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.rio.Rio;
+import org.folio.ld.dictionary.model.Resource;
+import org.folio.rdf4ld.mapper.Rdf4LdMapper;
+import org.folio.rdf4ld.model.ResourceMapping;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class Rdf4LdServiceImpl implements Rdf4LdService {
+
+  private final Rdf4LdMapper rdf4LdMapper;
+
+  @Override
+  public Set<Resource> mapToLd(InputStream input, String contentType, ResourceMapping mappingProfile) {
+    var model = readModel(input, contentType);
+    return rdf4LdMapper.mapToLd(model, mappingProfile);
+  }
+
+  @Override
+  public Set<Resource> mapToLdInstance(InputStream input, String contentType) {
+    var model = readModel(input, contentType);
+    return rdf4LdMapper.mapToLdInstance(model);
+  }
+
+  private static Model readModel(InputStream input, String contentType) {
+    if (isNull(input)) {
+      throw new IllegalArgumentException("Input stream is null");
+    }
+
+    var rdfFormat = Rio.getParserFormatForMIMEType(contentType)
+      .orElseThrow(() -> new IllegalArgumentException("Unsupported RDF format: " + contentType));
+
+    Model model;
+    try {
+      model = Rio.parse(input, "", rdfFormat);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Unreadable RDF data", e);
+    } catch (RDFParseException e) {
+      throw new IllegalArgumentException("RDF parsing error", e);
+    }
+    return model;
+  }
+
+}
