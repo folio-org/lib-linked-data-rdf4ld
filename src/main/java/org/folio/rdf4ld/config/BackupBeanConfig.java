8ed4272ab0504588f37049ad8a7f3eff5bfd19e9
Ryan Lee <rlee1@ebsco.com>
2025-09-05 11:10:34 -0700

    Move deserializer registration to bean post processing config

--- src/main/java/org/folio/rdf4ld/config/BackupBeanConfig.java
+++ src/main/java/org/folio/rdf4ld/config/BackupBeanConfig.java
@@ -3,12 +3,10 @@ package org.folio.rdf4ld.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.LongFunction;
 import org.folio.ld.dictionary.model.Resource;
-import org.folio.rdf4ld.util.ExportedResourceDeserializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,13 +17,10 @@ public class BackupBeanConfig {
   @Bean
   @ConditionalOnMissingBean
   public ObjectMapper objectMapper() {
-    SimpleModule module = new SimpleModule();
-    module.addDeserializer(Resource.class, new ExportedResourceDeserializer());
     return new ObjectMapper()
       .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
       .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-      .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
-      .registerModule(module);
+      .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
   }
 
   @Bean
