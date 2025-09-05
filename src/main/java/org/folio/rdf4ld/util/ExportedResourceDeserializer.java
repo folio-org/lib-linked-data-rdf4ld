bb6b116c93ab2dcf202e5a80c3eb6f021f771380
Ryan Lee <rlee1@ebsco.com>
2025-09-04 18:39:08 -0700

    Refine deserializing, and shift related tests to an integration test in order to use ObjectMapper

--- src/main/java/org/folio/rdf4ld/util/ExportedResourceDeserializer.java
+++ src/main/java/org/folio/rdf4ld/util/ExportedResourceDeserializer.java
@@ -2,34 +2,28 @@ package org.folio.rdf4ld.util;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import java.io.IOException;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.stream.StreamSupport;
 import org.folio.ld.dictionary.PredicateDictionary;
 import org.folio.ld.dictionary.ResourceTypeDictionary;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.ld.dictionary.model.ResourceEdge;
+import org.springframework.stereotype.Component;
 
 /**
  * Convert database export JSON to Resource.
  */
-public class ExportedResourceDeserializer extends StdDeserializer<Resource> {
+@Component
+public class ExportedResourceDeserializer extends JsonDeserializer<Resource> {
 
   private static final String FIELD_ID = "id";
   private static final String FIELD_LABEL = "label";
   private static final String FIELD_DOC = "doc";
   private static final String FIELD_TYPES = "types";
   private static final String FIELD_OUTGOING_EDGES = "outgoingEdges";
-
-  public ExportedResourceDeserializer() {
-    this(null);
-  }
-
-  public ExportedResourceDeserializer(Class<?> vc) {
-    super(vc);
-  }
 
   @Override
   public Resource deserialize(JsonParser jp, DeserializationContext context) throws IOException {
@@ -50,36 +44,36 @@ public class ExportedResourceDeserializer extends StdDeserializer<Resource> {
   }
 
   private void deserializeId(JsonNode node, Resource resource) {
-    if (node.has(FIELD_ID)) {
+    if (node.hasNonNull(FIELD_ID)) {
       resource.setId(node.get(FIELD_ID).asLong());
     }
   }
 
   private void deserializeLabel(JsonNode node, Resource resource) {
-    if (node.has(FIELD_LABEL)) {
+    if (node.hasNonNull(FIELD_LABEL)) {
       resource.setLabel(node.get(FIELD_LABEL).asText());
     }
   }
 
   private void deserializeDoc(JsonNode node, Resource resource) {
-    if (node.has(FIELD_DOC) && node.get(FIELD_DOC).isObject()) {
-      var doc = node.get(FIELD_DOC);
+    if (node.hasNonNull(FIELD_DOC) && node.get(FIELD_DOC).isObject()) {
+      var doc = node.get(FIELD_DOC).deepCopy();
       resource.setDoc(doc);
     }
   }
 
   private void deserializeTypes(JsonNode node, Resource resource) {
-    if (node.has(FIELD_TYPES) && node.get(FIELD_TYPES).isArray()) {
+    if (node.hasNonNull(FIELD_TYPES) && node.get(FIELD_TYPES).isArray()) {
       var types = node.withArray(FIELD_TYPES);
       StreamSupport.stream(types.spliterator(), false)
-        .map(type -> ResourceTypeDictionary.fromUri(type.asText()))
-        .filter(Optional::isPresent)
-        .forEach(dictType -> resource.addType(dictType.get()));
+        .map(type -> ResourceTypeDictionary.fromUri(type.asText()).orElse(null))
+        .filter(Objects::nonNull)
+        .forEach(resource::addType);
     }
   }
 
   private void deserializeEdges(JsonNode node, Resource resource) {
-    if (node.has(FIELD_OUTGOING_EDGES) && node.get(FIELD_OUTGOING_EDGES).isObject()) {
+    if (node.hasNonNull(FIELD_OUTGOING_EDGES) && node.get(FIELD_OUTGOING_EDGES).isObject()) {
       var edges = node.withObject(FIELD_OUTGOING_EDGES);
       edges.propertyStream()
         .filter(entry -> entry.getValue().isArray())
