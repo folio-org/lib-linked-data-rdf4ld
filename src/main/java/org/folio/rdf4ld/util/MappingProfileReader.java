8cb65dd798e1f4d2b08e1a4cd50365ef826efcf3
Ryan Lee <rlee1@ebsco.com>
2025-11-18 13:28:08 -0800

    Add a new profile configuration for admin metadata

--- src/main/java/org/folio/rdf4ld/util/MappingProfileReader.java
+++ src/main/java/org/folio/rdf4ld/util/MappingProfileReader.java
@@ -32,6 +32,7 @@ public class MappingProfileReader {
   public static final String MANUFACTURE = "provision/manufacture.json";
   public static final String PRODUCTION = "provision/production.json";
   public static final String PUBLICATION = "provision/publication.json";
+  public static final String ADMIN_METADATA = "admin_metadata/admin_metadata.json";
   private final Rdf4LdObjectMapper objectMapper;
 
   public ResourceMapping getBibframe20Profile() {
@@ -52,6 +53,7 @@ public class MappingProfileReader {
         readResourceMapping(MANUFACTURE).ifPresent(im.getResourceMapping()::addOutgoingEdgesItem);
         readResourceMapping(PRODUCTION).ifPresent(im.getResourceMapping()::addOutgoingEdgesItem);
         readResourceMapping(PUBLICATION).ifPresent(im.getResourceMapping()::addOutgoingEdgesItem);
+        readResourceMapping(ADMIN_METADATA).ifPresent(im.getResourceMapping()::addOutgoingEdgesItem);
         getWorkMapping().ifPresent(im.getResourceMapping()::addOutgoingEdgesItem);
         return im;
       });
